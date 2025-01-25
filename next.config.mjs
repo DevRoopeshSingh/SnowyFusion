556bf1408b00c3b5e15841e9d115c06c78a2077a
Roopesh <roopeshsingh993@gmail.com>
2025-01-25 19:21:48 +0530

    made changes next config to push the build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Enables React's Strict Mode for development
+  experimental: {
+    appDir: false, // Ensure the app directory isn't used (as you're using `pages`)
+  },
+  images: {
+    domains: ["example.com"], // Replace with domains for external images if applicable
+  },
+  trailingSlash: false, // Adjust based on your URL preferences
+};
 
 export default nextConfig;
+  
